fe2eb8629fabb7eb79698f542205df8840c1306f
Chloe <chloetsohy@gmail.com>
2026-09-24 16:23:08 -0400

    Updated comments for each code line

--- Variables/variables-challenge/js/script.js
+++ Variables/variables-challenge/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Look at the bird!
- * Chloe Tso, Charbel John Dagher,
+ * Chloe Tso, Charbel John Dagher
  * 
  * This project showcases the use of variables to create a simple animation of a bird flying across the screen. 
  * The bird's position and color are controlled by variables, and the background color changes over 
@@ -22,6 +22,7 @@ let bird = {
   y4: 180,
   speed: 0.1,
 
+  // Colour
   fill: {
     r: 255,
     g: 255,
@@ -41,11 +42,14 @@ let mrFurious = {
     gb: 225,
   }
 };
+
+// shake variables for Mr. Furious
 let shake = {
-  x: 200,
-  y: 200
+  shaker1: 200,
+  shaker2: 200
 };
 
+// sky colour variables
 let sky = {
   r: 173,
   g: 216,
@@ -63,23 +67,32 @@ function setup() {
  * Draw (and update) Mr. Furious
  */
 function draw() {
+
+  // sky changing colour
   background(sky.r, sky.g, sky.b);
   if (sky.b > 0) {
     sky.b -= 0.5;
     sky.g -= 0.5;
     sky.r -= 0.5;
   }
-  mrFurious.x = random(shake.x, shake.y);
-  mrFurious.y = random(shake.x, shake.y);
 
-  if (shake.x > 170) {
-    shake.x -= 0.1;
-    shake.y += 0.1;
+
+  // shaker for mrFurious
+  mrFurious.x = random(shake.shaker1, shake.shaker2);
+  mrFurious.y = random(shake.shaker1, shake.shaker2);
+
+  // incremental shaking of mrFurious when he gets angrier
+  if (shake.shaker1 > 170) {
+    shake.shaker1 -= 0.1;
+    shake.shaker2 += 0.1;
     mrFurious.size += 0.5;
   }
+
+
   push();
   noStroke();
 
+  // changes mrFurious's colour to redder as he gets angrier
   fill(mrFurious.fill.r, mrFurious.fill.gb, mrFurious.fill.gb);
   if (mrFurious.fill.gb > 70) {
     mrFurious.fill.gb -= 0.5;
@@ -91,9 +104,12 @@ function draw() {
 
   push();
   noStroke();
+  // colour for bird
   fill(bird.fill.r, bird.fill.g, bird.fill.b);
+  // shape for bird
   quad(bird.x1, bird.y1, bird.x4, bird.y4, bird.x2, bird.y2, bird.x3, bird.y3);
 
+  // changes bird speed as he flies across the screen, and makes his wings flap in a sine wave pattern
   if (bird.x1 < 400) {
     bird.x1 += bird.speed;
     bird.x2 += bird.speed;
